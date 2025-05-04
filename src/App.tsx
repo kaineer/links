ea703ad7fef4cc93b7a88e42995f27ec982e5fa4
Sergey Kluchkovsky <kaineer@gmail.com>
2025-05-04 20:12:54 +0500

    Сохраняет изменения

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { linkPage } from "../package.json"
+import { LinkPage } from './pages/LinkPage';
+import avatarUrl from './assets/avatar.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { profile, links,  icons } = linkPage;
+
+  profile.url = avatarUrl;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <LinkPage
+      profile={ profile }
+      links={ links }
+      icons={ icons }
+    />
   )
 }
 
